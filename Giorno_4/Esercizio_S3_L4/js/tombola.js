e293b9717aa72c9ff5e748d7ce4ac89dc5650134
Ciraldos <andreaciraldo2@gmail.com>
2024-04-11 14:19:16 +0200

    new file:   Giorno_4/Esercizio_S3_L4/js/tombola.js 	new file:   Giorno_4/css/styles.css 	new file:   Giorno_4/index.html 	new file:   Giorno_4/js/index.js

---
+++ Giorno_4/Esercizio_S3_L4/js/tombola.js
@@ -0,0 +1,20 @@
+const generateMainBoard = function(){
+    //funziona che crea il tabellone
+}
+
+const fillArray = function() {
+    // inserire in un array 
+}
+
+const getRandomNum = function() {
+    //generare un numero random
+}
+
+const generateRandNUmber = function() {
+    //stampare il numero 
+    // associare la classe..
+}
+
+const generateUserBoards = function () {
+    // genera e gestisce le tabelline
+}
