53bfbcfa02316126e29adc3614e48e116450ac34
Ciraldos <andreaciraldo2@gmail.com>
2024-04-11 16:59:42 +0200

    new file:   Giorno_4/Esercizio_S3_L4/css/style.css 	new file:   Giorno_4/Esercizio_S3_L4/index.html 	modified:   Giorno_4/Esercizio_S3_L4/js/tombola.js

--- Giorno_4/Esercizio_S3_L4/js/tombola.js
+++ Giorno_4/Esercizio_S3_L4/js/tombola.js
@@ -1,20 +1,48 @@
 const generateMainBoard = function(){
     //funziona che crea il tabellone
+    const tableDiv = document.getElementById("table");
+    for(let i = 0; i < 90; i++) {
+        const numDiv = document.createElement("div");
+        numDiv.classList.add("cell");
+        numDiv.innerText = i + 1;
+        tableDiv.appendChild(numDiv);
+    }
 }
+
 
 const fillArray = function() {
     // inserire in un array 
+    const arr = []
+    for (let i = 0; i < 90; i++) {
+      arr.push(i + 1)
+    }
+    return arr
 }
 
-const getRandomNum = function() {
-    //generare un numero random
-}
+const getRandomNum = function (range) {
+    // generare un numero random
+    const i = Math.floor(Math.random() * range.length)
+    const random = range.splice(i, 1)[0]
+    return random
+  }
+  
 
-const generateRandNUmber = function() {
-    //stampare il numero 
+const generateRandNumber = function(range) {
+    const numRandom = getRandomNum(range);
+    
+    // Associa la funzione all'evento click del pulsante
+    numGeneratorButton.addEventListener("click", function() {
+        const ranNum = getRandomNum(); // Ottieni un numero casuale
+
+
+        const randomNumberElement = document.getElementById("number"); // Ottieni l'elemento in cui mostrare il numero
+        randomNumberElement.innerText = ranNum;})
     // associare la classe..
 }
 
 const generateUserBoards = function () {
     // genera e gestisce le tabelline
 }
+
+generateRandNumber()
+generateMainBoard()
